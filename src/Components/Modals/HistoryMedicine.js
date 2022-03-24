00c2706b8cda461e0e72e097b43311b2916e8bcb
Jaivin <jaivinvenkolla@gmail.com>
2022-03-24 07:44:15 -0400

    patient history

---
+++ src/Components/Modals/HistoryMedicine.js
@@ -0,0 +1,100 @@
+import React from 'react'
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import Paper from '@mui/material/Paper';
+
+function HistoryMedicine({ setShowMedicines, selectedList }) {
+    return (
+        <div className="Modal_Container" style={{ maxWidth: '90%' }}>
+            <div className="row  mobile-ls">
+                <div className="col-md-8 ">
+                    {/* <h4 className="underline"><strong>Add Patient</strong></h4> */}
+                </div>
+                <div style={{ cursor: 'pointer' }} onClick={() => { setShowMedicines(false) }} className="col-md-4  d-flex justify-content-end align-items-end">
+                    <h4><i class="fa fa-window-close" aria-hidden="true"></i></h4>
+                </div>
+            </div>
+            <div className="addPatient navbar-light " style={{ backgroundColor: "#FFFFFF", border: '' }}>
+                <div className="row" >
+                    <TableContainer component={Paper}>
+                        <Table sx={{ minWidth: 650 }} aria-label="simple table">
+                            <TableHead>
+                                <TableRow>
+                                    <TableCell >SI No</TableCell>
+                                    <TableCell >Medicine Name</TableCell>
+                                    <TableCell >Pharmacological Name</TableCell>
+                                    <TableCell >Medicine Type</TableCell>
+                                    <TableCell >Dosage</TableCell>
+                                    <TableCell >Duration</TableCell>
+                                    <TableCell >Quantity</TableCell>
+                                </TableRow>
+                            </TableHead>
+                            {selectedList?.map((item, index) => {
+                                return (
+                                    <TableRow>
+                                        <TableCell component="th" scope="row">
+                                            {index + 1}.
+                                        </TableCell>
+                                        <TableCell >{item._med_id.med_name}</TableCell>
+                                        <TableCell >{item._med_id.s_med_name}</TableCell>
+                                        <TableCell >{item._med_id.med_type}</TableCell>
+                                        <TableCell >{item.dosage}</TableCell>
+                                        <TableCell >{item.days}</TableCell>
+                                        <TableCell >{item.total_quantity}</TableCell>
+                                    </TableRow>
+                                )
+                            })}
+                            {/* // <Accordion style={{ width: '100%' }} className="space-x-5" defaultActiveKey={['0']} > */}
+                            {/* // <Accordion.Item eventKey={i} onClick={() => { handleClick(item.priscriptionId) }}> */}
+
+                            {/* // <Accordion.Header className="" > */}
+
+                            {/* {primaryDetails?.map((item, i) => {
+                                return (
+                                    <>
+                                        <TableBody>
+                                            <TableRow>
+                                                <TableCell component="th" scope="row">
+                                                    {i + 1}.
+                                                </TableCell>
+                                                <TableCell >{item.hId}</TableCell>
+                                                <TableCell >{item.hPhone}</TableCell>
+                                                <TableCell >{item.dName}</TableCell>
+                                                <TableCell >{item.dPhone}</TableCell>
+                                                <TableCell >{item.app_date}</TableCell>
+                                                <TableCell >{item.app_time}</TableCell>
+                                                <TableCell ><IoIosArrowForward /></TableCell>
+                                            </TableRow>
+                                        </TableBody>
+                                        {selectedList?.map((item, index) => {
+                                            return (
+                                                <TableRow>
+                                                    <TableCell component="th" scope="row">
+                                                        {index + 1}.
+                                                    </TableCell>
+                                                    <TableCell >{item._med_id.med_name}</TableCell>
+                                                    <TableCell >{item._med_id.s_med_name}</TableCell>
+                                                    <TableCell >{item._med_id.med_type}</TableCell>
+                                                    <TableCell >{item.dosage}</TableCell>
+                                                    <TableCell >{item.days}</TableCell>
+                                                    <TableCell >{item.total_quantity}</TableCell>
+                                                </TableRow>
+                                            )
+                                        })}
+                                    </>
+                                )
+                            })} */}
+                        </Table>
+                    </TableContainer>
+
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default HistoryMedicine
--- src/Components/PatientHistory.js
+++ src/Components/PatientHistory.js
@@ -11,22 +11,36 @@ import { useNavigate } from 'react-router-dom';
 import { IoMdArrowRoundBack } from 'react-icons/io'
 import instance from '../config/api'
 import { useLocation } from 'react-router-dom'
+import { Accordion } from 'react-bootstrap'
+
+import PropTypes from 'prop-types';
+import Box from '@mui/material/Box';
+import Collapse from '@mui/material/Collapse';
+import IconButton from '@mui/material/IconButton';
+import Typography from '@mui/material/Typography';
+import CollapsibleTable from './MiTable';
+import { IoIosArrowForward } from 'react-icons/io'
+import HistoryMedicine from './Modals/HistoryMedicine';
+// import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
+// import KeyboardArrowUpIcon from '@mui/icons-material/KeyboardArrowUp';
+
 
 
 
 function PatientHistory() {
     const hospitalId = localStorage.getItem('HospitalId')
     const [prescriptionHistoryList, setPrescriptionHistoryList] = useState([])
+    const [selectedList, setSelectedList] = useState([])
+    const [primaryDetails, setPrimaryDetails] = useState([])
+    const [showMedicines, setShowMedicines] = useState(false)
     const state = useLocation()
     const patientId = state?.state
-    console.log('patientId', patientId)
     const navigate = useNavigate();
     const backButtonHandler = () => {
         navigate('/taskDashboard')
     }
 
     useEffect(() => {
-        console.log('mounting');
         let mount = true;
         fetchPatientHistory()
         return mount = false
@@ -39,28 +53,76 @@ function PatientHistory() {
             _pat_id: patientId
         }
         instance.post('/list_patient_prescription', obj).then((res) => {
-            let resValue = []
-            // let newData = [...prescriptionHistoryList]
+            var dummy = []
+            let allDetails = []
             for (let i = 0; i < res?.data.prescription?.length; i++) {
+                console.log('prs--', res.data.prescription);
+                const hId = res.data.prescription[i]._hos_id.name
+                const hPhone = res.data.prescription[i]._hos_id.contact_1
+                const app_date = res.data.prescription[i]._pat_app_id.app_date
+                const app_time = res.data.prescription[i]._pat_app_id.app_time
+                const dName = res.data.prescription[i]._refer_doc_id.doc_name
+                const dPhone = res.data.prescription[i]._refer_doc_id.doc_contact
+                const priscriptionId = res?.data.prescription[i]._id
+                const newObj = {
+                    hId, hPhone, app_date, app_time, dName, dPhone, priscriptionId
+                }
+                allDetails.push(newObj)
+                const refDoctorName = res.data.prescription[i]._refer_doc_id.doc_name
                 const patientHistoryDetails = {
                     _pat_presc_id: res?.data.prescription[i]._id
                 }
-                console.log('id--', patientHistoryDetails);
                 instance.post('/list_patient_prescription_dosage', patientHistoryDetails).then((response) => {
-                    console.log('response----', response.data.dosage);
-                    for (let j = 0; j < response?.data.dosage.length; j++) {
-                        resValue.push(response?.data.dosage[j]);
-                        // newData = response?.data.dosage[j]
-                        // setPrescriptionHistoryList(newData)
+                    console.log('response.data.dosage', response.data.dosage);
+                    dummy = [...dummy, response.data.dosage]
+                    var newArr = [];
+                    for (var j = 0; j < dummy.length; j++) {
+                        newArr = newArr.concat(dummy[j]);
+                        newArr[j].refDoc = refDoctorName
                     }
+                    setPrescriptionHistoryList(newArr);
                 })
             }
-            console.log('resValue',resValue);
-            setPrescriptionHistoryList(resValue);
+            setPrimaryDetails(allDetails)
+        })
+    }
+    // console.log('prescriptionHistoryList', prescriptionHistoryList);
+
+    const handleClick = (id) => {
+        const patientHistoryDetails = {
+            _pat_presc_id: id
+        }
+        console.log(id);
+        instance.post('/list_patient_prescription_dosage', patientHistoryDetails).then((response) => {
+            // console.log('response.data.dosage', response.data.dosage);
+            setSelectedList(response.data.dosage)
+            setShowMedicines(true)
         })
     }
 
-    console.log('length---', prescriptionHistoryList.length)
+    function createData(name, calories, fat, carbs, protein, price) {
+        return {
+            name,
+            calories,
+            fat,
+            carbs,
+            protein,
+            price,
+            history: [
+                {
+                    date: '2020-01-05',
+                    customerId: '11091700',
+                    amount: 3,
+                },
+                {
+                    date: '2020-01-02',
+                    customerId: 'Anonymous',
+                    amount: 1,
+                },
+            ],
+        };
+    }
+
 
     return (
         <div >
@@ -78,48 +140,80 @@ function PatientHistory() {
                 </div>
             </div>
             <div className="navbar-light  m-5 ">
-                {prescriptionHistoryList?.map((item, index) => {
-                    return (
-                        <div className="row mt-3">
-                            <div className="col-md-12 d-flex justify-content-center">
-                                <TableContainer component={Paper}>
-                                    <Table sx={{ minWidth: 650 }} aria-label="simple table">
-                                        <TableHead>
-                                            <TableRow>
-                                                <TableCell >SI No</TableCell>
-                                                <TableCell >Medicine Name</TableCell>
-                                                <TableCell >Pharmacological Name</TableCell>
-                                                <TableCell >Type</TableCell>
-                                                <TableCell >Dosage</TableCell>
-                                                <TableCell >Duration</TableCell>
-                                                <TableCell >Quantity</TableCell>
-                                                <TableCell >Comments</TableCell>
-                                            </TableRow>
-                                        </TableHead>
-                                        <TableBody>
-                                            {/* {item?.map((val, i) => {
-                                                
+                <div className="row mt-3">
+                    <div className="col-md-12 d-flex justify-content-center">
+                        <TableContainer component={Paper}>
+                            <Table sx={{ minWidth: 650 }} aria-label="simple table">
+                                <TableHead>
+                                    <TableRow>
+                                        <TableCell >SI No</TableCell>
+                                        <TableCell >Hospital</TableCell>
+                                        <TableCell >Hospital Phone Number</TableCell>
+                                        <TableCell >Doctor Name</TableCell>
+                                        <TableCell >Doctor Phone Number</TableCell>
+                                        <TableCell >Appointment Date</TableCell>
+                                        <TableCell >Appointment Time</TableCell>
+                                        <TableCell >Prescription Medicines</TableCell>
+
+                                    </TableRow>
+                                </TableHead>
+                                {/* // <Accordion style={{ width: '100%' }} className="space-x-5" defaultActiveKey={['0']} > */}
+                                {/* // <Accordion.Item eventKey={i} onClick={() => { handleClick(item.priscriptionId) }}> */}
+
+                                {/* // <Accordion.Header className="" > */}
+
+                                {primaryDetails?.map((item, i) => {
+                                    return (
+                                        <>
+                                            <TableBody>
+                                                <TableRow>
+                                                    <TableCell component="th" scope="row">
+                                                        {i + 1}.
+                                                    </TableCell>
+                                                    <TableCell >{item.hId}</TableCell>
+                                                    <TableCell >{item.hPhone}</TableCell>
+                                                    <TableCell >{item.dName}</TableCell>
+                                                    <TableCell >{item.dPhone}</TableCell>
+                                                    <TableCell >{item.app_date}</TableCell>
+                                                    <TableCell >{item.app_time}</TableCell>
+                                                    <TableCell ><IoIosArrowForward cursor='pointer' onClick={() => { handleClick(item.priscriptionId) }} size={20} /></TableCell>
+                                                </TableRow>
+                                            </TableBody>
+                                            {/* {selectedList?.map((item, index) => {
+                                                return (
                                                     <TableRow>
                                                         <TableCell component="th" scope="row">
-                                                            {i + 1}
+                                                            {index + 1}.
                                                         </TableCell>
-                                                        <TableCell >{val._med_id.med_name}</TableCell>
-                                                        <TableCell >{val._med_id.s_med_name}</TableCell>
-                                                        <TableCell >{val._med_id.med_type}</TableCell>
-                                                        <TableCell >{val.dosage}</TableCell>
-                                                        <TableCell >{val.days}</TableCell>
-                                                        <TableCell >{val.total_quantity}</TableCell>
+                                                        <TableCell >{item._med_id.med_name}</TableCell>
+                                                        <TableCell >{item._med_id.s_med_name}</TableCell>
+                                                        <TableCell >{item._med_id.med_type}</TableCell>
+                                                        <TableCell >{item.dosage}</TableCell>
+                                                        <TableCell >{item.days}</TableCell>
+                                                        <TableCell >{item.total_quantity}</TableCell>
                                                     </TableRow>
-                                                
+                                                )
                                             })} */}
-                                        </TableBody>
-                                    </Table>
-                                </TableContainer>
+                                        </>
+                                    )
+                                })}
+                            </Table>
+                        </TableContainer>
+                        {/* //  </Accordion.Header> */}
+                        {/* //  <Accordion.Body> */}
+                        {/* //  </Accordion.Body> */}
 
+                        {/* // </Accordion.Item> */}
+                        {/* //  </Accordion> */}
+                        {/* <CollapsibleTable primaryDetails={primaryDetails} /> */}
+                        {showMedicines ?
+                            <div className="centered loginWrapper d-flex justify-content-center align-items-center">
+                                <HistoryMedicine setShowMedicines={setShowMedicines} selectedList={selectedList} />
                             </div>
-                        </div>
-                    )
-                })}
+                            : null}
+                    </div>
+                </div>
+
             </div>
         </div>
     )
